a4243dfd4a3616cb4912eec670e4f3f3ab833bbf
Michael Lai <michaelrlai8@gmail.com>
2023-06-10 21:57:44 -0700

    added NavBar component, home and products routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+
+import Home from './pages/Home';
+import Products from './pages/Products';
+
 function App() {
-  return <div className='App'>Hello {process.env.REACT_APP_TEST}</div>;
+  return (
+    <div className='App'>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/products' element={<Products />} />
+        <Route />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
