7e677201f390129d78e6fb38fc9028a161bef543
Michael Lai <michaelrlai8@gmail.com>
2023-06-12 11:11:50 -0700

    removes Transitions import from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 import { Route, Routes } from 'react-router-dom';
-import { Transition } from '@headlessui/react';
 
 import NavBar from './components/NavBar';
 
