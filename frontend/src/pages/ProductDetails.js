622500f6fa9a6435a687b9cf751f091b118c2da4
Michael Lai <michaelrlai8@gmail.com>
2023-06-21 02:18:59 -0700

    added third variant of first shoes

--- frontend/src/pages/ProductDetails.js
+++ frontend/src/pages/ProductDetails.js
@@ -79,7 +79,7 @@ const ProductDetails = () => {
     }
   `;
 
-  const { error, data } = useQuery(GET_PRODUCT, {
+  const { loading, error, data } = useQuery(GET_PRODUCT, {
     variables: { id },
   });
 
@@ -113,131 +113,140 @@ const ProductDetails = () => {
 
   return (
     <div className='mt-[60px]'>
+      {loading && <div className='lg:mx-12'>Loading</div>}
       {data && (
-        <div className='w-screen lg:flex'>
-          <div className='relative w-full lg:ml-12'>
-            <img
-              src={`${process.env.REACT_APP_API_URL}${data.product.data.attributes.images.data[currentPhotoIndex].attributes.url}`}
-              alt=''
-              className='w-full'
-            />
-            <div className='absolute right-6 top-6 flex gap-3 text-2xl'>
-              <button onClick={previousPhoto}>
-                <IoChevronBack />
-              </button>
-              <button onClick={nextPhoto}>
-                <IoChevronForward />
-              </button>
-            </div>
-          </div>
-
-          <div className='w-full pt-6 lg:w-2/3 lg:pt-0'>
-            <div className='px-6 lg:px-12'>
-              <div className='text-3xl'>
-                {data.product.data.attributes.name}
-              </div>
-              <div className=''>
-                {`${capitalizeFirst(
-                  data.product.data.attributes.category.data.attributes.name
-                )}'s`}
-              </div>
-              <div>{`$${data.product.data.attributes.price}`}</div>
-              <div className='mt-6 grid grid-cols-5 gap-2'>
-                {data.product.data.attributes.variants.data.map((variant) => (
-                  <Link
-                    onClick={() => setSelectedSize(0)}
-                    to={`/products/${variant.id}`}
-                    key={variant.attributes.colorway}
-                    className='relative'
-                  >
-                    <img
-                      src={`${process.env.REACT_APP_API_URL}${variant.attributes.images.data[0].attributes.url}`}
-                      alt=''
-                    />
-                    <div
-                      className={`absolute left-0 top-0 h-full w-full border-solid border-black hover:border ${
-                        variant.id === data.product.data.id ? 'border' : ''
-                      }`}
-                    ></div>
-                  </Link>
-                ))}
-              </div>
-              <div className='mt-6'>
-                <div>Sizes</div>
-              </div>
-              <div className='mt-2 grid grid-cols-5 gap-2'>
-                {data.product.data.attributes.sizes.map((size) => (
-                  <button
-                    onClick={() => setSelectedSize(size)}
-                    key={size}
-                    className={`border border-solid py-2 text-center hover:border-black ${
-                      selectedSize === size ? 'border-black' : 'border-gray-200'
-                    }`}
-                  >
-                    {size}
-                  </button>
-                ))}
-              </div>
-              <div className='mt-2'>
-                {data.product.data.attributes.colorway}
-              </div>
-              <div className='mt-6 text-center'>
-                <button className='w-full bg-black py-4 text-white hover:bg-gray-500'>
-                  ADD TO BAG
+        <div>
+          <div className='w-screen lg:flex'>
+            <div className='relative w-full lg:ml-12'>
+              <img
+                src={`${process.env.REACT_APP_API_URL}${data.product.data.attributes.images.data[currentPhotoIndex].attributes.url}`}
+                alt=''
+                className='w-full'
+              />
+              <div className='absolute right-6 top-6 flex gap-3 text-2xl'>
+                <button onClick={previousPhoto}>
+                  <IoChevronBack />
+                </button>
+                <button onClick={nextPhoto}>
+                  <IoChevronForward />
                 </button>
               </div>
+            </div>
 
-              <div className='mt-6 border-t border-gray-200 py-6'>
-                <div
-                  onClick={() => setShowSizeGuide(!showSizeGuide)}
-                  className='flex justify-between hover:cursor-pointer'
-                >
-                  <div className='text-xl'> Sizing Guide</div>
-                  <div className='flex items-center pr-2 text-2xl'>
-                    {showSizeGuide ? <IoChevronUp /> : <IoChevronDown />}
-                  </div>
+            <div className='w-full pt-6 lg:w-2/3 lg:pt-0'>
+              <div className='px-6 lg:px-12'>
+                <div className='text-3xl'>
+                  {data.product.data.attributes.name}
                 </div>
-                {showSizeGuide && (
-                  <div className='pt-6'>
-                    <div>
-                      1. Step on a piece of paper with your heel slightly
-                      touching a wall behind.
-                    </div>
-                    <div>
-                      2. Mark the end of your longest toe on the paper (you
-                      might need a friend to help you) and measure from the wall
-                      to the marking.
-                    </div>
-                    <div>
-                      3. Do the same for the other foot and compare measurements
-                      with our size chart to get the right size.
-                    </div>
-                  </div>
-                )}
+                <div className=''>
+                  {`${capitalizeFirst(
+                    data.product.data.attributes.category.data.attributes.name
+                  )}'s`}
+                </div>
+                <div>{`$${data.product.data.attributes.price}`}</div>
+                <div className='mt-6 grid grid-cols-5 gap-2'>
+                  {data.product.data.attributes.variants.data.map((variant) => (
+                    <Link
+                      onClick={() => setSelectedSize(0)}
+                      to={`/products/${variant.id}`}
+                      key={variant.attributes.colorway}
+                      className='relative'
+                    >
+                      <img
+                        src={`${process.env.REACT_APP_API_URL}${variant.attributes.images.data[0].attributes.url}`}
+                        alt=''
+                      />
+                      <div
+                        className={`absolute left-0 top-0 h-full w-full border-solid border-black hover:border ${
+                          variant.id === data.product.data.id ? 'border' : ''
+                        }`}
+                      ></div>
+                    </Link>
+                  ))}
+                </div>
+                <div className='mt-6'>
+                  <div>Sizes</div>
+                </div>
+                <div className='mt-2 grid grid-cols-5 gap-2'>
+                  {data.product.data.attributes.sizes.map((size) => (
+                    <button
+                      onClick={() => setSelectedSize(size)}
+                      key={size}
+                      className={`border border-solid py-2 text-center hover:border-black ${
+                        selectedSize === size
+                          ? 'border-black'
+                          : 'border-gray-200'
+                      }`}
+                    >
+                      {size}
+                    </button>
+                  ))}
+                </div>
+                <div className='mt-2'>
+                  {data.product.data.attributes.colorway}
+                </div>
+                <div className='mt-6 text-center'>
+                  <button className='w-full bg-black py-4 text-white hover:bg-gray-500'>
+                    ADD TO BAG
+                  </button>
+                </div>
 
-                <div className='mt-6 border-b border-t border-gray-200 py-6'>
+                <div className='mt-6 border-t border-gray-200 py-6'>
                   <div
-                    onClick={() => setShowShippingPolicy(!showShippingPolicy)}
+                    onClick={() => setShowSizeGuide(!showSizeGuide)}
                     className='flex justify-between hover:cursor-pointer'
                   >
-                    <div className='text-xl'> Shipping & Returns</div>
+                    <div className='text-xl'> Sizing Guide</div>
                     <div className='flex items-center pr-2 text-2xl'>
-                      {showShippingPolicy ? <IoChevronUp /> : <IoChevronDown />}
+                      {showSizeGuide ? <IoChevronUp /> : <IoChevronDown />}
                     </div>
                   </div>
-                  {showShippingPolicy && (
+                  {showSizeGuide && (
                     <div className='pt-6'>
-                      Free standard shipping on orders $50+ and free 60-day
-                      returns
+                      <div>
+                        1. Step on a piece of paper with your heel slightly
+                        touching a wall behind.
+                      </div>
+                      <div>
+                        2. Mark the end of your longest toe on the paper (you
+                        might need a friend to help you) and measure from the
+                        wall to the marking.
+                      </div>
+                      <div>
+                        3. Do the same for the other foot and compare
+                        measurements with our size chart to get the right size.
+                      </div>
                     </div>
                   )}
+
+                  <div className='mt-6 border-b border-t border-gray-200 py-6'>
+                    <div
+                      onClick={() => setShowShippingPolicy(!showShippingPolicy)}
+                      className='flex justify-between hover:cursor-pointer'
+                    >
+                      <div className='text-xl'> Shipping & Returns</div>
+                      <div className='flex items-center pr-2 text-2xl'>
+                        {showShippingPolicy ? (
+                          <IoChevronUp />
+                        ) : (
+                          <IoChevronDown />
+                        )}
+                      </div>
+                    </div>
+                    {showShippingPolicy && (
+                      <div className='pt-6'>
+                        Free standard shipping on orders $50+ and free 60-day
+                        returns
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
+          <Footer />
         </div>
       )}
-      <Footer />
     </div>
   );
 };
